657b0dc3cbb562f8a090073d4d8a56bac5bc9014
Julio De Bastiani <julioc.debastiani@gmail.com>
2019-10-30 21:50:46 -0300

    Base CMake

---
+++ ascii-neural-net/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "ascii-neural-net/t.hpp"
+
+int main()
+{
+    std::cout << "IT LIVES!!!!!!" << std::endl;
+    test_func();
+    return 0;
+}
---
+++ ascii-neural-net/t.cpp
@@ -0,0 +1,8 @@
+#include "ascii-neural-net/t.hpp"
+
+#include <iostream>
+
+void test_func()
+{
+    std::cout << "Test f" << std::endl
+;}
